00269e51873dd6c5d6982787e54ac8628a6f55a5
後閑諒一 <nargok@gmail.com>
2017-12-02 22:34:55 +0900

    create task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171202130510) do
+ActiveRecord::Schema.define(version: 20171202133129) do
 
   create_table "projects", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
     t.boolean "done", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
+    t.boolean "done", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
