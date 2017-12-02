a4a323e015f12d2b6b576e75ab2d1aadb193c9ff
後閑諒一 <nargok@gmail.com>
2017-12-03 00:30:34 +0900

    create tasks_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root to: 'projects#index'
   resources :projects
 
+  namespace :api, format: 'json' do
+    resources :tasks, only: [:index, :create, :update]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
