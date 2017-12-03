766a4c823b3c793ec8f744fb2820760898049b59
後閑諒一 <nargok@gmail.com>
2017-12-03 13:26:08 +0900

    navbar

--- app/javascript/packs/todo.js
+++ app/javascript/packs/todo.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
+import Header from './components/header.vue'
 
 new Vue({
     el: "#app",
     data: {
         todo: "",
         tasks: ['風呂そうじ', '洗濯', '炊事']
+    },
+    components: {
+        'navbar': Header,
     }
-})
+});
